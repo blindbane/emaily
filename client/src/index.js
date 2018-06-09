61c60d7c20257a666083ec058e367a0ce1148944
Aidan Bane <aidanfbane@gmail.com>
2018-06-09 00:53:00 -0700

    (feat) Redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import {render} from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 import App from './components/App'
 
-render(<App />, document.getElementById('root'))
+const store = createStore(() => [], {}, applyMiddleware())
+
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+render(<Root />, document.getElementById('root'))
