364d4fa7627cc37777197ab4ad59f465e34ca8b6
Aidan Bane <aidanfbane@gmail.com>
2018-06-09 04:23:57 -0700

    (feat) Header conditional render logic

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 
 class Header extends Component {
+  renderContent () {
+    switch (this.props.auth) {
+      case null:
+        return null
+      case false:
+        return <li><a href='/auth/google'>Login with Google</a></li>
+      default:
+        return <a href='/api/logout'>Logout</a>
+    }
+  }
   render () {
+    console.log('auth:', this.props)
     return (
       <nav>
         <div className='nav-wrapper'>
           <a href='/' className='left brand-logo'>Emaily</a>
           <ul id='nav-mobile' className='right'>
-            <li><a href='sass.html'>Login with Google</a></li>
+            <li><a>{this.renderContent()}</a></li>
           </ul>
         </div>
       </nav>
-
     )
   }
 }
 
-export default Header
+const mapStateToProps = (state) => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps)(Header)
