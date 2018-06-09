c4d24613b1f4a53a9149d9ff29c7f67e4bcefe32
Aidan Bane <aidanfbane@gmail.com>
2018-06-09 01:43:50 -0700

    (feat) Add materialize-css for styling

---
+++ client/src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+  render () {
+    return (
+      <nav>
+        <div class='nav-wrapper'>
+          <a href='/' class='left brand-logo'>Emaily</a>
+          <ul id='nav-mobile' class='right'>
+            <li><a href='sass.html'>Login with Google</a></li>
+          </ul>
+        </div>
+      </nav>
+
+    )
+  }
+}
+
+export default Header
