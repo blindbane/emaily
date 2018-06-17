af78c19e9c6e225b81574559ec78343ab419a6b9
Aidan Bane <aidanfbane@gmail.com>
2018-06-16 20:45:15 -0700

    (feat) Add auth middleware for payments

--- routes/billing.js
+++ routes/billing.js
@@ -1,8 +1,8 @@
 const keys = require('../config/keys')
 const stripe = require('stripe')(keys.stripeSecretKey)
-
+const auth = require('../middleware/auth')
 module.exports = app => {
-  app.post('/api/stripe', async (req, res) => {
+  app.post('/api/stripe', auth, async (req, res) => {
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
